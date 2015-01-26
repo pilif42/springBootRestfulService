cd330b86a2d3b10b760cc0095ffee8cf8712a986
Philippe Brossier <Philippe.Brossier@ordnancesurvey.co.uk>
2015-01-26 17:58:32 +0000

    solving the lo4j config issue

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -1,8 +1,8 @@
 package controller;
 
 import domain.Greeting;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,14 +13,18 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final Logger logger = LogManager.getLogger(GreetingController.class.getName());
+    private static final Logger logger = LogManager.getLogger(GreetingController.class);
     private static final String template = "Hello, %s!";
 
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value="/greeting", method=RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        logger.debug("entering greeting...");
+        logger.info("info: entering greeting...");
+        logger.debug("debug: entering greeting...");
+        logger.warn("warn: entering greeting...");
+        logger.error("error : You chose a number > 100 ");
+        logger.fatal("fatal : You chose a number > 100 ");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
