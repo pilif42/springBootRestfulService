d6b6f14557499fe666fa0e96d525472a12320172
Philippe Brossier <Philippe.Brossier@ordnancesurvey.co.uk>
2015-01-26 17:30:48 +0000

    configuration for log4j2

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import domain.Greeting;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +13,14 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
+    private static final Logger logger = LogManager.getLogger(GreetingController.class.getName());
     private static final String template = "Hello, %s!";
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value="/greeting", method=RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        logger.debug("entering greeting...");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
