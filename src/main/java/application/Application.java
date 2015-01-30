f4f173d77adee440c11f83d3d5c45bb583a2c2e5
Philippe Brossier <Philippe.Brossier@ordnancesurvey.co.uk>
2015-01-30 21:37:38 +0000

    solving issue with autowiring - still pb coz no datasource defined

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("controller")
+@ComponentScan({"controller","domain","repository","service"})
 @EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
